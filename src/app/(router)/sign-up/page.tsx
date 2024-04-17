f838f201b3c1982a0199500f2b9c8bf325c50a14
Blackraven93 <reblackraven@gmail.com>
2024-04-18 03:58:46 +0900

    style(view): 약관 동의를 위한 폼 제작

--- src/app/(router)/sign-up/page.tsx
+++ src/app/(router)/sign-up/page.tsx
@@ -1,30 +1,14 @@
 import { Container, PIXRHeader } from "@/app/_containers";
-import { Checkbox } from "@/app/_ui";
+import { Form } from "@/app/_ui";
 
 export default function Page({}) {
   return (
     <Container>
       <PIXRHeader />
       <div className="w-full h-calc-header bg-default flex justify-center items-center">
-        <div className="flex flex-col justify-center text-center gap-10">
+        <div className="flex flex-col justify-center text-center gap-10 w-[300px]">
           <h1 className="text-brown-900 h1-700-32">약관에 동의가 필요해요</h1>
-          <form action="" method="post">
-            <div>
-              <Checkbox id={"all"}>전체동의 선택항목에 대한 동의 포함</Checkbox>
-            </div>
-            <div>
-              <Checkbox id={"all"}>전체동의 선택항목에 대한 동의 포함</Checkbox>
-            </div>
-            <div>
-              <Checkbox id={"all"}>전체동의 선택항목에 대한 동의 포함</Checkbox>
-            </div>
-            <div>
-              <Checkbox id={"all"}>전체동의 선택항목에 대한 동의 포함</Checkbox>
-            </div>
-            <div>
-              <Checkbox id={"all"}>전체동의 선택항목에 대한 동의 포함</Checkbox>
-            </div>
-          </form>
+          <Form />
         </div>
       </div>
     </Container>
