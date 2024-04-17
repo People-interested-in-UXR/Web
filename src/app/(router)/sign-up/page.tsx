8c1d78987fc2e73daec0a561321b52e96d063d63
Blackraven93 <reblackraven@gmail.com>
2024-04-17 22:18:01 +0900

    style(view): sign in 화면 구현 및 sign up 마크업 생성

--- src/app/(router)/sign-up/page.tsx
+++ src/app/(router)/sign-up/page.tsx
@@ -1,5 +1,32 @@
-import { Container } from "@/app/_containers";
+import { Container, PIXRHeader } from "@/app/_containers";
+import { Checkbox } from "@/app/_ui";
 
 export default function Page({}) {
-  return <Container>Sign-up</Container>;
+  return (
+    <Container>
+      <PIXRHeader />
+      <div className="w-full h-calc-header bg-default flex justify-center items-center">
+        <div className="flex flex-col justify-center text-center gap-10">
+          <h1 className="text-brown-900 h1-700-32">약관에 동의가 필요해요</h1>
+          <form action="" method="post">
+            <div>
+              <Checkbox id={"all"}>전체동의 선택항목에 대한 동의 포함</Checkbox>
+            </div>
+            <div>
+              <Checkbox id={"all"}>전체동의 선택항목에 대한 동의 포함</Checkbox>
+            </div>
+            <div>
+              <Checkbox id={"all"}>전체동의 선택항목에 대한 동의 포함</Checkbox>
+            </div>
+            <div>
+              <Checkbox id={"all"}>전체동의 선택항목에 대한 동의 포함</Checkbox>
+            </div>
+            <div>
+              <Checkbox id={"all"}>전체동의 선택항목에 대한 동의 포함</Checkbox>
+            </div>
+          </form>
+        </div>
+      </div>
+    </Container>
+  );
 }
