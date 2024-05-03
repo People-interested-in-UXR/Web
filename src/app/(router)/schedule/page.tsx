27e65f11c7bcba4358814eb773da30099e2d8246
Blackraven93 <reblackraven@gmail.com>
2024-05-04 02:30:32 +0900

    style(view): 가입 완료 화면 개발

---
+++ src/app/(router)/schedule/page.tsx
@@ -0,0 +1,10 @@
+import { Container, PIXRHeader } from "@/app/_containers";
+
+export default function Page({}) {
+  return (
+    <Container>
+      <PIXRHeader />
+      dd
+    </Container>
+  );
+}
