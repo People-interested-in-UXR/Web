7e29168a16d2f2d53f0b7bf8a99c437e0d753cdd
Blackraven93 <reblackraven@gmail.com>
2024-11-04 18:32:03 +0900

    refactor(image): api upload image resizing

--- src/app/_ui/_atomics/Board/Board.tsx
+++ src/app/_ui/_atomics/Board/Board.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 
 import { Description } from "../Description";
 
--- src/app/api/s3/notion/cover/route.ts
+++ src/app/api/s3/notion/cover/route.ts
@@ -1,5 +1,7 @@
 import { createServer } from "@/app/utils/supabase";
+
 import { NextResponse } from "next/server";
+import sharp from "sharp";
 import { v4 as uuid } from "uuid";
 
 export async function POST(request: Request) {
@@ -7,10 +9,19 @@ export async function POST(request: Request) {
   const formData = await request.formData();
   const file = formData.get("cover") as File;
 
+  const buffer = await file.arrayBuffer();
+  const optimizedImageBuffer = await sharp(Buffer.from(buffer))
+    .resize(503, 272) //* 2배수
+    .webp({ quality: 90 })
+    .toBuffer();
+  const optimizedImageFile = new File([optimizedImageBuffer], file.name, {
+    type: "image/webp",
+  });
+
   const supabase = await createServer();
   const { data, error } = await supabase.storage
     .from("image")
-    .upload(`notion_cover/${uuidImageName}`, file);
+    .upload(`notion_cover/${uuidImageName}`, optimizedImageFile);
   if (error) {
     console.error("파일이 업로드 되지 않습니다.", error);
     return NextResponse.json(
